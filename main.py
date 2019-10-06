fb019ee74439931d5ea4c49eb5bf91296f7be9a5
tyg04308 <tyg04308@naver.com>
2019-10-06 23:29:24 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#import classifier.AbuseClassifier as aClif
+from classifier.AbuseClassifier import AbuseClassifier
 from sendmail.sendmail import send_warning
 
-#clif = aClif.AbuseClassifier('sports_model/sports.model', 'sports_model/lb.pickle')
-#result = clif.isAbusedChild('input/boxing1.mp4')
+clif = AbuseClassifier('model_path', 'lb_path')
+result = clif.isAbusedChild('video_path')
 
-test = {1: [52752.70003, 54421.03333], 2: [59459.4, 64197.4666], 3: [79512.76666, 81781.7]}
-send_warning("rlaehdwn1026@gmail.com", "YOUR PASSWORD",  "tyg04308@naver.com", test)
+send_warning("rlaehdwn1026@gmail.com", "YOUR PASSWORD",  "tyg04308@naver.com", result)
 
-#print ('----------result-----------')
-#print (result)
+print ("Finish")
